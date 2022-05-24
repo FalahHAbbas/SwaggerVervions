76d0da18e6be454ebae2d6cec830ffdce37ac7b6
falah <FalahH.Abbas@gmail.com>
2022-05-25 01:01:49 +0300

    ..

---
+++ Controllers/GroupD/AclassController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggerVervions.Controllers.GroupD;
+
+[Route("api/Path1/[controller]/[action]")]
+[ApiController]
+public class AclassController : ControllerBase {
+
+    [HttpPost]
+    public async Task<IActionResult> GetAll() {
+        var result =  new{
+            Name = "sdf",
+            Code = "dfsd",
+            Description = "sdfd",
+            Status = "sdfsd",
+        };
+        return Ok(result);
+    }
+}
